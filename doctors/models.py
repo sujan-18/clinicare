4eee46a17358d28bcc399eb1978a3eff5fca684c
sujan shrestha <newarsujan2323@gmail.com>
2026-06-20 14:32:45 +0545

    this is my first commit about project setup

---
+++ doctors/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Doctor(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=15)
+    specialization = models.CharField(max_length=100)
+    years_of_experience = models.IntegerField()
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.specialization}"
